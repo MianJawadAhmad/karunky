cc0efbd2e9e6ad926e4217f96988ecf37eca5da7
Mian Aizaz <48237975+MianAizaz6@users.noreply.github.com>
2019-07-16 17:35:42 +0500

    first

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,15 +1,17 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation'
+import HomeScreen from '../Containers/HomeScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = createStackNavigator({
+  HomeScreen: { screen: HomeScreen },
   LaunchScreen: { screen: LaunchScreen }
 }, {
   // Default config for all screens
   headerMode: 'none',
-  initialRouteName: 'LaunchScreen',
+  initialRouteName: 'HomeScreen',
   navigationOptions: {
     headerStyle: styles.header
   }
